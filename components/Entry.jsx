3cb2d3e8b8652a29efc0f935b7936ee9aa565989
lolodev <horaciodev1706@gmail.com>
2025-08-05 16:16:22 -0300

    Pass props to Entry component

--- components/Entry.jsx
+++ components/Entry.jsx
@@ -1,17 +1,26 @@
-export default function Entry() {
+export default function Entry(props) {
   return (
-    <article>
-      <div className="image">
-        <img src="../images/mountfuji.jpg" alt="mountfuji" />
-        <p className="credit">Foto de <a href="https://unsplash.com/es/@kevin_charit?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Kevin Charit</a>
-        </p>
+    <article className="journal-entry">
+      <div className="main-image-container">
+        <img
+          className="main-image"
+          src={props.image.src}
+          alt={props.image.alt}
+        />
       </div>
-      <div className="text">
-        <h3><img src="../images/marker.png" alt="marker" /> JAPAN <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu" >View on Google Maps</a></h3>
-        <h2>Mount Fuji</h2>
-        <h4>12 Jan, 2021 - 24 Jan, 2021</h4>
-        <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+      <div className="info-container">
+        <img
+          className="marker"
+          src="../images/marker.png"
+          alt="map marker icon"
+        />
+        <span className="country">{props.country}</span>
+        <a href={props.googleMapsLink}>View on Google Maps</a>
+        <h2 className="entry-title">{props.title}</h2>
+        <p className="trip-dates">{props.dates}</p>
+        <p className="entry-text">{props.text}</p>
       </div>
+
     </article>
   );
 }
